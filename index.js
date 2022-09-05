cb56d34a9fdcf1e9415db450a48fd3d9e6aa6075
Juwon <dnladesanya1@gmail.com>
2022-09-05 16:57:51 +0100

    add js to hamburger button

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".nav-toggle");
+const navigator = document.querySelectorAll(".nav-list > a");
+hamburger.addEventListener("click", () => {
+    document.body.classList.toggle ("active")
+})
+
+
+
+navigator.forEach(links => {
+   links.addEventListener('click', ()=>{
+    document.body.classList.remove('active')
+   }) 
+});
